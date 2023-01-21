4784fd338923236000965d398024b34f342904ed
Garvinus <117110312+Garvinus@users.noreply.github.com>
2023-01-21 20:13:43 +0100

    layout header/footer

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,7 +1,23 @@
+import Link from 'next/link';
+import Styles from './Layout.module.css';
+
 export function Layout(props) {
   return (
     <div>
-      <header>Header</header>
+      <header className={Styles.header}>
+        <div className={Styles.logo}>Logo</div>
+        <nav className={Styles.nav_bar}>
+          <Link className={Styles.nav_link} href="">
+            Link
+          </Link>
+          <Link className={Styles.nav_link} href="">
+            Link
+          </Link>
+          <Link className={Styles.nav_link} href="">
+            Link
+          </Link>
+        </nav>
+      </header>
       <main>{props.children}</main>
       <footer>Footer</footer>
     </div>
