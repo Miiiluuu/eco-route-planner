084f51c2bc7334eec5f39c1a0d157d7a18ae4049
Sivinia-Josephine <112758754+Sivinia-Josephine@users.noreply.github.com>
2023-02-19 17:23:48 +0100

    change text and style

--- components/About/About.js
+++ components/About/About.js
@@ -1,19 +1,21 @@
-import Styles from "./About.module.css";
+import styles from './About.module.css';
 
 export function About(props) {
   return (
-    <div className="background">{props.children}
-      <div className="text-background">
-      <h1 className="h1-about">Über uns</h1>
-      <p className="p-about">TraWell hat es sich zum Ziel gesetzt, die Vereinbarkeit von Nachhaltigkeit und guter Reiseplanung zu vereinen und
-         so grünes Reisen für alle zu ermöglichen. Dabei legen wir darauf Wert, dass alle sonst nicht aufgeführten Alternativen 
-         schnell und übersichtlich dargestellt werden. Sind dir unsere Nachhaltigkeitsblätter aufgefallen? Diese geben dir einen 
-         einen schnellen Überblick darüber, ob deine Route wenig oder mehr Emissionen verursachen würde. <br/>
-         Wir danken dir ganz herzlich für die Nutzung, denn nur, wenn wir alle gemeinsam arbeiten, können wir unseren 
-         Planeten retten. Empfehle uns gerne weiter und folge uns auf unseren Social-Media-Kanälen, um keins von unseren Projekten 
-         zu verpassen!
-      </p>
-     </div> 
+    <div className={styles.background}>
+      {props.children}
+      <div className={styles['text-background']}>
+        <h1 className={styles['h1-about']}>Über uns</h1>
+        <p className={styles['p-about']}>TraWell has set itself the goal of promoting the compatibility of sustainability and good travel planning
+          to enable green travel for everyone. We attach great importance to the fact that all alternatives not otherwise listed
+          be displayed quickly and clearly. Have you noticed our sustainability sheets? These give you one
+          a quick overview of whether your route would cause fewer or more emissions. <br/>
+          We thank you very much for using it, because we can only do ours if we all work together
+          save planet. Feel free to recommend us and follow us on our social media channels to avoid any of our projects
+          to miss!
+        </p>
+        </div> 
+      <div className={styles.footer}></div>
     </div>
   );
 }
