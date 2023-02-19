d983375ccca5572822dde3c5253bdb93bc04ae41
Mieke Möller <mieke.moeller56@gmail.com>
2023-02-19 20:32:03 +0100

    finished the trip overview
    - dynamically displays individual legs of the trips
    - right now just for the first transit itinerary
    - TODO: transportation icons don´t have the same size...

--- components/TripDisplay/TripDisplay.js
+++ components/TripDisplay/TripDisplay.js
@@ -1,18 +1,20 @@
-import { useState } from 'react';
+import { useState, Fragment } from 'react';
 import Styles from './TripDisplay.module.css';
 
 // this function extracts informations from the individually legs of the travel plan
-const ExtractInfo = leg => {
-  console.log("Info leg", leg)
-  const legMode = 'mode' in leg ? leg.mode : '';
-  const time = 'duration' in leg ? leg.duration : '';
-  console.log("Modus", legMode);
-  console.log("duration", time);
-  return `${legMode}`;
+const extractInfo = info => {
+  console.log('Info leg', info);
+  const mode = 'mode' in info ? info.mode : 'unavailable';
+  const time = 'duration' in info ? info.duration : 'unavailable';
+  console.log('duration', time);
+  console.log('mode', mode);
+  return 'test';
 };
 
 export default function TripDisplay({ planBicycle, planCar, planTransit }) {
   const [show, setShow] = useState(false); // hook to show the details of the trip
+  // some features:
+  const duration = planTransit.itineraries?.[1]?.duration; // duration transit trip No1 in sec
 
   // if the user clicks on a trip then more details are shown and the route is displayed on the map
   const onSuggestHandler = () => {
@@ -30,39 +32,71 @@ export default function TripDisplay({ planBicycle, planCar, planTransit }) {
     // just show this element if all informations are fetched (maybe there is a shorter way to express that?)
     return (
       <>
+        {/* display transit trip(s) */}
         <div
           className={Styles.single_trip}
           onClick={() => onSuggestHandler()}
           tabIndex="-1" // By default an <div> element has no focus so onBlur usually doesn't work, this is a workaround but not recommended. Using a button instead would be the right way
           onBlur={() => setShow(false)}
         >
+          {/* emission specification */}
           <div className={Styles.icon_leafs}>
             <img src="leaf_icon.png" alt="leaf icon" />
             <img src="leaf_icon.png" alt="leaf icon" />
           </div>
+
+          {/* transportation&time specification */}
           <div className={Styles.icon_transportation_time}>
             <img src="transit_icon.png" alt="transit icon" />
             <div className={Styles.trip_time}>
-              {`${Math.round(planTransit.itineraries?.[0]?.duration / 60)} min`}
+              {`${Math.round(duration / 60)} min`}
             </div>
           </div>
+
+          {/* trip overview */}
           <div className={Styles.trip_overview}>
-            <div className={Styles.circle}></div>
-
-            {planTransit.itineraries?.[1]?.legs.map((leg, index) => (
-              <>
-              <ExtractInfo leg={leg}/>
-                <div className={`${Styles.mode} line`} key={index}>
-                  test
-                </div>
-              </>
-            ))}
-
-            {/* <div className={Styles.line}></div> */}
+            {planTransit.itineraries?.[1]?.legs.map((leg, index) => {
+              extractInfo(leg);
+              console.log('Mode inside JSX: ', leg.mode);
+              return (
+                <Fragment key={index}>
+                  <div className={Styles.transportation_icon_container}>
+                    <img
+                      className={Styles.transportation_icon}
+                      src={
+                        leg.mode == 'WALK'
+                          ? 'walk_icon.png'
+                          : 'transit_icon.png'
+                      }
+                      alt="transportation icon"
+                    />
+                  </div>
+                  <div className={`${leg.mode}`}>
+                    {/* inline styling with Styled JSX (CSS-in-JS library) */}
+                    <style jsx>{`
+                      .${leg.mode} {
+                        height: 4px;
+                        display: flex;
+                      }
+                    `}</style>
+                    {/* the dynamic style has a separate <style jsx> tag so that only the dynamic parts are recomputed when props change. */}
+                    <style jsx>{`
+                      .${leg.mode} {
+                        background-color: ${leg.mode == 'WALK'
+                          ? '#e2d784'
+                          : '#4CAF50'};
+                        width: ${leg.duration}px;
+                      }
+                    `}</style>
+                  </div>
+                </Fragment>
+              );
+            })}
             <div className={Styles.circle}></div>
           </div>
         </div>
-        {show && <div className={Styles.trip_details}>show it!!!</div>}
+
+        {show && <div className={Styles.trip_details}>show details!!!</div>}
       </>
     );
   }
